8f583d0e141d9c0c905e37e55150890d5887aabf
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2022-12-02 14:09:31 +0900

    리액트 앱에서 발생하는 에러

---
+++ error-catch/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "./User";
+
+function App() {
+  const user = {
+    id: 1,
+    useranme: "velopert",
+  };
+  return <User />;
+}
+
+export default App;
---
+++ error-catch/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://dd1ffca1f47045a8bdce8948768e313c@o4504257608941568.ingest.sentry.io/4504257610514432",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
