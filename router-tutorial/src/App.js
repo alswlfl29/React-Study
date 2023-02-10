f6c1ebcdab5bda0ecc0d8dae4f6658a087ee29ac
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2023-02-10 13:52:23 +0900

    라우터 부가기능

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,14 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profile from "./Profile";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
