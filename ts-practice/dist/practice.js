c2b8a234180599604a6e46da5b5df17a3c6bba10
minjilee <budlalswl1129@gmail.com>
2023-02-20 18:30:49 +0900

    인터페이스 사용

--- ts-practice/dist/practice.js
+++ ts-practice/dist/practice.js
@@ -1,14 +1,23 @@
 "use strict";
-let count = 0; //숫자
-count += 1;
-count = "갑자기 분위기 문자열"; //에러 발생
-const message = "hello world"; //문자열
-const done = true; //불리언 값
-const numbers = [1, 2, 3]; //숫자 배열
-const messages = ["hello", "world"]; //문자열 배열
-message.push(1); //에러 발생 -> 숫자는 안됨
-let mightBeUndefined = undefined; //string/undefined 둘중에 하나
-let nullableNumber = null; // number/null 둘중에 하나
-let color = "red"; //red/orange/yellow 셋 중에 하나
-color = "yellow";
-color = "green"; //3개말고 다른거면 에러
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    // 너비 가져오는 함수 구현
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const shapes = [new Circle(5), new Rectangle(10, 5)];
+shapes.forEach((shape) => {
+    console.log(shape.getArea());
+});
--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,19 +1,41 @@
-let count = 0; //숫자
-count += 1;
-count = "갑자기 분위기 문자열"; //에러 발생
+// shape 라는 interface 선언
+interface Shape {
+  getArea(): number;
+  //Shape interface에는 getArea라는 함수가 꼭 존재해야하고 반환값는 숫자이어야함
+}
 
-const message = "hello world"; //문자열
+class Circle implements Shape {
+  //`implements`키워드 사용하여 해당 클래스가 Shape interface의 조건 충족하겠다고 명시
 
-const done: boolean = true; //불리언 값
+  constructor(public radius: number) {
+    this.radius = radius;
+  }
 
-const numbers: number[] = [1, 2, 3]; //숫자 배열
-const messages: string[] = ["hello", "world"]; //문자열 배열
+  // 너비 가져오는 함수 구현
+  getArea(): number {
+    return this.radius * this.radius * Math.PI;
+  }
+}
 
-message.push(1); //에러 발생 -> 숫자는 안됨
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {
+    this.width = width;
+    this.height = height;
+  }
 
-let mightBeUndefined: string | undefined = undefined; //string/undefined 둘중에 하나
-let nullableNumber: number | null = null; // number/null 둘중에 하나
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
 
-let color: "red" | "orange" | "yellow" = "red"; //red/orange/yellow 셋 중에 하나
-color = "yellow";
-color = "green"; //3개말고 다른거면 에러
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+console.log(rectangle.width); //불가능
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
