9960c32fecb5a2b0b5ad6932aeaee8e4ff5dcc9c
minjilee <budlalswl1129@gmail.com>
2023-02-24 00:14:40 +0900

    일반 객체에서 인터페이스 사용

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,41 +1,19 @@
-// shape 라는 interface 선언
-interface Shape {
-  getArea(): number;
-  //Shape interface에는 getArea라는 함수가 꼭 존재해야하고 반환값는 숫자이어야함
+// 일반객체에서의 인터페이스 사용
+interface Person {
+  name: string;
+  age?: number;
+}
+interface Developer extends Person {
+  skills: string[];
 }
 
-class Circle implements Shape {
-  //`implements`키워드 사용하여 해당 클래스가 Shape interface의 조건 충족하겠다고 명시
+const person: Person = {
+  name: "김사람",
+  age: 20,
+};
+const expert: Developer = {
+  name: "김개발",
+  skills: ["javascript", "react"],
+};
 
-  constructor(public radius: number) {
-    this.radius = radius;
-  }
-
-  // 너비 가져오는 함수 구현
-  getArea(): number {
-    return this.radius * this.radius * Math.PI;
-  }
-}
-
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
-
-  getArea(): number {
-    return this.width * this.height;
-  }
-}
-
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-console.log(rectangle.width); //불가능
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach((shape) => {
-  console.log(shape.getArea());
-});
+const people: Person[] = [person, expert];
